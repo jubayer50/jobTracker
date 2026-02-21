4db94014c980bb526aa1ce738d2eff5159c0b157
jubayer50 <aujubayer1@gmail.com>
2026-02-22 01:19:50 +0600

    design done with responsive

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// get the count three buttons
+let totalCount = document.getElementById("totalCount");
+let interviewCount = document.getElementById("interviewCount");
+let rejectedCount = document.getElementById("rejectedCount");
+
+// get the three buttons
+let allTabBtn = document.getElementById("all-tab-btn");
+let interviewTabBtn = document.getElementById("interview-tab-btn");
+let rejectedTabBtn = document.getElementById("rejected-tab-btn");
